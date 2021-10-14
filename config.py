9859fbd4ae08403124a722acb2f69f7cc41c518b
Rashdan Luthfy <rashdanml@gmail.com>
2021-10-13 22:39:54 -0600

    Created dev branch, made some changes to conf methods

--- config.py
+++ config.py
@@ -2,7 +2,6 @@
 import requests
 import time
 import json
-import random
 import socket
 import urllib3
 import configparser as cp
@@ -17,40 +16,48 @@ class Config():
         self.clientsecret = self.conf['DEFAULT']['clientsecret']
         self.oauthtoken = self.conf['DEFAULT']['oauthtoken']
 
-    def validate(self, try_number=1):
+    def validate(self):
         self.conf.read('config.ini')
         self.oauthtoken = self.conf['DEFAULT']['oauthtoken']
 
         validate_url = "https://id.twitch.tv/oauth2/validate"
         valid_headers = {'client-id': self.clientid, 'Authorization': 'OAuth ' + self.oauthtoken}
 
-        try:
-            valid = requests.get(validate_url, headers=valid_headers).json()
-        except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError, socket.gaierror):
-            time.sleep(15)
-            return self.validate(self, try_number=try_number+1)
-        else:
-            return valid
+        i = 0
+        tries = 100
+        while i < tries:
+            i += 1
+            try:
+                valid = requests.get(validate_url, headers=valid_headers).json()
+            except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError, socket.gaierror):
+                time.sleep(15)
+                continue
+            else:
+                return valid
 
-    def refresh(self, try_number=1):
+    def refresh(self):
         auth_url = "https://id.twitch.tv/oauth2/token"
         auth_params = {'client_id': self.clientid, 'client_secret': self.clientsecret,
                        'grant_type': 'client_credentials'}
 
-        try:
-            key = requests.post(auth_url, params=auth_params).json()
-        except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError, socket.gaierror):
-            time.sleep(15)
-            return self.refresh(self, try_number=try_number+1)
-        else:
-            self.oauthtoken = key['access_token']
-            self.conf['DEFAULT']['oauthtoken'] = self.oauthtoken
+        i = 0
+        tries = 100
+        while i < tries:
+            i += 1
+            try:
+                key = requests.post(auth_url, params=auth_params).json()
+            except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError, socket.gaierror):
+                time.sleep(15)
+                continue
+            else:
+                self.oauthtoken = key['access_token']
+                self.conf['DEFAULT']['oauthtoken'] = self.oauthtoken
 
-            with open('config.ini', 'w') as file:
-                self.conf.write(file)
+                with open('config.ini', 'w') as file:
+                    self.conf.write(file)
 
-            file.close()
-            return key
+                file.close()
+                return key
 
 
 conf = Config()
